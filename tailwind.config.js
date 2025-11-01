9c85afa4995c74217f661ab58c112ce09a27b219
Madabhushani Srikar Acharya <160750858+Srikar282006@users.noreply.github.com>
2025-11-01 11:27:07 +0530

    Reorder content paths in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+
   theme: {
     extend: {},
   },
